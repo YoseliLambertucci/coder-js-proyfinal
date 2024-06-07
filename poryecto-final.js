3394b1b6f65b5a88484d515190d43e1821f5ee3a
YoseliLambertucci <yoselilambertucci@gmail.com>
2024-06-07 10:15:41 -0300

    agregue que se guarde en un array

---
+++ poryecto-final.js
@@ -0,0 +1,48 @@
+
+
+const botonEnviar = document.getElementById("button");
+botonEnviar.addEventListener("click", registrarproveedores);
+
+function registrarproveedores() {
+    const formulario = document.getElementById("formulario");
+    const datosProveedores = {
+        nombre: formulario.nombre.value,
+        celular: formulario.celular.value,
+        email: formulario.email.value,
+        direccion: formulario.direccion.value,
+        actividad: formulario.actividad.value,
+        tema: formulario.tema.value,
+        extra: formulario.extra.value,
+    };
+
+    const DatosJason = JSON.stringify(datosProveedores);
+
+    localStorage.setItem("proveedor", DatosJason);
+    function GuardarArray() {
+        const TomarDatos = localStorage.getItem("proveedor");
+        if(TomarDatos){
+            const datosProveedores = JSON.parse(TomarDatos);
+            const guardarDatos = [];
+            guardarDatos.push(datosProveedores);
+            console.log(guardarDatos);
+        }
+        }
+    
+    }
+
+    
+    function GuardarArray() {
+        const TomarDatos = localStorage.getItem("proveedor");
+        if(TomarDatos){
+            const datosProveedores = JSON.parse(TomarDatos);
+            const guardarDatos = [];
+            guardarDatos.push(datosProveedores);
+            console.log(guardarDatos);
+        }
+        }
+    
+    GuardarArray()
+
+
+
+
